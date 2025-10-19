dfe392ca4d31a26cb6f06d4ccf395e7710868c85
Nikita Silaev <Beeeirym@gmail.com>
2025-10-19 15:45:31 +0300

    feat: Implementing a database interaction layer

--- src/config.py
+++ src/config.py
@@ -6,6 +6,13 @@ class Settings(BaseSettings):
     # Telegram Bot
     TELEGRAM_BOT_TOKEN: str
 
+    # Database
+    DB_HOST: str
+    DB_PORT: int
+    DB_NAME: str
+    DB_USER: str
+    DB_PASSWORD: str
+
     class Config:
         env_file = Path(__file__).parent / ".env"
 
